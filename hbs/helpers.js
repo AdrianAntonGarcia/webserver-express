df1a99411d9dcdbcfa7960c503aae3a5bb782307
Adrian Anton <adrtler@gmail.com>
2020-02-10 22:08:54 +0100

    Primer commit

---
+++ hbs/helpers.js
@@ -0,0 +1,24 @@
+const hbs = require('hbs');
+//Helpers
+hbs.registerHelper('getAnio', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('capitalizar', (texto) => {
+    let palabras = texto.split(' ');
+    palabras.forEach((palabra, idx) => {
+        palabras[idx] = palabra.charAt(0).toUpperCase() + palabra.slice(1).toLowerCase();
+    });
+
+    return palabras.join(' ');
+});
+
+
+hbs.registerHelper('objeto', () => {
+    let objeto = {
+        nombre: 'adrian',
+        edad: 24
+    };
+
+    return objeto;
+});
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+require('./hbs/helpers');
+
+
+const port = process.env.PORT || 3000;
+
+
+//Para servir una pagina web
+app.use(express.static(__dirname + '/public'));
+//Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'adrián'
+    });
+
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+    // res.render('about', {
+    //     nombre: 'Adrián',
+    //     anio: new Date().getFullYear()
+    // });
+
+});
+
+app.get('/data', (req, res) => {
+    res.send('Hola data');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
